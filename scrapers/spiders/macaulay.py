a91df43609a2dc94880d574dc67dd0fafe6b86e7
Mark Henderson <henderson.mark@gmail.com>
2020-09-25 22:47:11 -0400

    Updating scraper to loop through all assets

--- scrapers/spiders/macaulay.py
+++ scrapers/spiders/macaulay.py
@@ -11,62 +11,46 @@ import scrapy
 class MacaulayLibrarySpider(scrapy.Spider):
     name = 'macaulaylibrary'
     allowed_domains = ['macaulaylibrary.org']
-    start_urls = ['http://macaulaylibrary.org/sitemap.xml']
+
+    URL_TEMPLATE = 'https://macaulaylibrary.org/asset/{}'
+    start_asset_id = 1
+    max_asset_id = 10
 
     URLTAG = "{http://www.sitemaps.org/schemas/sitemap/0.9}url"
     LOCTAG = "{http://www.sitemaps.org/schemas/sitemap/0.9}log"
 
-
     # Step 1:
-    # Get urls from http://macaulaylibrary.org/sitemap.xml
-    def parse(self, response):
-        # generate etree from response string: 
-        # http://www.fis.unipr.it/doc/python-lxml-2.2.3/doc/html/tutorial.html
-        root = etree.fromstring(response.body)
-
-        # Navigate XML subtree and fetch URL strings
-        children = [sitemap.getchildren()[0] for sitemap in root]
-        url_tags = filter(lambda child: child.tag == self.URLTAG, children)
-        urls = [url.getchildren()[0].text for url in url_tags]
-        xml_urls = filter(lambda url: ".xml" in url, urls)
-
-        # Queue requests and call parse_audio_sitemap against responses
-        for url in xml_urls:
-            yield scrapy.Request(url, callback=self.parse_audio_sitemap)
-
-    # Step 2:
-    # Get individual media sitemaps
-    def parse_audio_sitemap(self, response):
-        root = etree.fromstring(response.body)
-        children = [sitemap.getchildren()[0] for sitemap in root]
-        urls = [child.text for child in children]
-        
-        for url in urls:
+    # Start the scraper by making a big array of all the urls from
+    # start asset_id to max_asset_id, then concurrently requesting them
+    def start_requests(self):
+        for asset_id in range(self.start_asset_id, self.max_asset_id):
+            url = self.URL_TEMPLATE.format(asset_id)
             yield scrapy.Request(url, callback=self.parse_media_page)
 
-    # Step 3:
-    # Get JSON-LD schema.org metadata from each individual page (187,000+ pages)
-    #
-    # TODO: Write response.
-    #
+
+    # Step 2:
+    # Get JSON-LD schema.org metadata from each individual page, as well as an
+    # untested derivation of the media URL.
     def parse_media_page(self, response):
         h = html.fromstring(response.body)
         json_ld_element = h.cssselect("script[type='application/ld+json']")[0]
         json_ld = json.loads(json_ld_element.text)
 
         # Only capture resources that have valid geo-coordinates
-        json_ld["commonName"] = h.cssselect(".SpecimenHeader-commonName span")[0].text
-        json_ld["sciName"] = h.cssselect(".SpecimenHeader-sciName")[0].text
-        
+        # json_ld["commonName"] = h.cssselect(".SpecimenHeader-commonName span")[0].text
+        # json_ld["sciName"] = h.cssselect(".SpecimenHeader-sciName")[0].text
+
         # Handle geospatial stuff
         geo = json_ld.pop("geo")
         json_ld["geo"] = {
             "lat": geo.get("latitude", ""),
             "lon": geo.get("longitude", "")
         }
-       
+
         # Remove any schema.org stuff that's left
         json_ld.pop("@type")
         json_ld.pop("@context")
+
+        json_ld["likely_media_url"] = json_ld["url"].replace("https://macaulaylibrary.org/asset/", "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/")
 
         yield json_ld
