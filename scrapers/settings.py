9a89c95fab093addb79c177511ced77a6abe50e5
Mark Henderson <henderson.mark@gmail.com>
2020-10-13 20:01:30 -0400

    setting cache expiry to 30 days

--- scrapers/settings.py
+++ scrapers/settings.py
@@ -84,7 +84,7 @@ ITEM_PIPELINES = {
 
 # Enable and configure HTTP caching (disabled by default)
 HTTPCACHE_ENABLED = True
-HTTPCACHE_EXPIRATION_SECS = 86400
+HTTPCACHE_EXPIRATION_SECS = 86400 * 30
 HTTPCACHE_DIR = 'httpcache'
 HTTPCACHE_IGNORE_HTTP_CODES = []
 HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
